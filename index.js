5062f8074a75df236e18752f11b26a7ca5fccb94
Matten0 <mattiahosola2@gmail.com>
2025-05-20 23:28:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

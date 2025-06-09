3cd12227825d3c00a4b9dd82b40237fb91acbdea
Matten0 <mattiahosola2@gmail.com>
2025-06-09 15:49:48 +0300

    misc

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
+    .then(() => {
         console.log('connected to MongoDB')
     })
     .catch((error) => {
@@ -21,8 +21,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true, "Password required"],
-        unique: [false]
+        required: [true, "Password required"]
     }
 })
 

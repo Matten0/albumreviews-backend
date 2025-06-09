373bacc6e645aba40d619b70041ff06721662ac8
Matten0 <mattiahosola2@gmail.com>
2025-06-09 23:18:28 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const jwt = require("jsonwebtoken")
 
 module.exports = async (request, response, next) => {
     try {
         const token = await request.headers.authorization.split(" ")[1]
         //console.log(token)
-        const decodedToken = jwt.verify(token, "RANDOM-TOKEN")
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET)
         //console.log(decodedToken)
         request.user = decodedToken
         next()
